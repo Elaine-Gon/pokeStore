b1f6573e9bb17c7adcb18e81066c2eca647aebce
Elaine-Gon <elanegoncalves2000@gmail.com>
2022-08-25 18:15:16 -0300

    consumption the api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pokeStore';
+
+  public pokemons$: Observable<any>;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.pokemons$ = this.http.get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=200")
+  }
 }
